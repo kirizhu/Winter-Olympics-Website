ca7d6a91472473326e08ae9dba59c93a2477e7e0
kirizhu <32507458+kirizhu@users.noreply.github.com>
2018-05-17 12:13:29 +0200

    12

--- resources/views 2/about.blade.php
+++
@@ -1,6 +0,0 @@
-<?php include "components/header.php" ?>
-<div class="container mt-5" style="height:800px; border:3px solid #ffca20"></div>
-
-
-
-<?php include "components/footer.php" ?>
--- resources/views 2/events.blade.php
+++
@@ -1,9 +0,0 @@
-<?php include "components/header.php" ?>
-
-<div class="container mt-5" style="height:800px; border:3px solid #ffca20">
-<?php include "components/carousel.php" ?>
-
-</div>
-
-
-<?php include "components/footer.php" ?>
