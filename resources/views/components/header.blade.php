415e2997dbde419384d4daa4ca76ecaefe2ebbec
kirizhu <32507458+kirizhu@users.noreply.github.com>
2018-05-17 10:40:27 +0200

    test

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="Group6" content="2Sweden">
-    <title>Welcome to Sweden</title>
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" ng-app="myApp">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+        <title>Rome2Rio API</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<?php include "components/header.blade.php" ?>
+ @include('components.header')
 
 
     <div class="container" style="margin-top:75px; margin-bottom: 85px">
