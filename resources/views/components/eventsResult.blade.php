078e7131f4efc29d6004ae9c5f8d2c600378022e
sculs <liusongscu@gmail.com>
2018-05-18 15:57:42 +0200

    small change

--- resources/views/components/eventsResult.blade.php
+++ resources/views/components/eventsResult.blade.php
@@ -1,57 +1,55 @@
 
 <!-- Result-->
 <div class="container mb-3">
-    <div class="col-12 box-temp" style="height: 500px;">
+    <div class="box-temp" style="height: 500px;">
+
+        <table class="table table-striped ">
+            <thead>
+                <tr id="table-head"></tr>
+                <tr id="table-row-1"></tr>
+                <tr id="table-row-2"></tr>
+                <tr id="table-row-3"></tr>
+                <tr id="table-row-4"></tr>
+                <tr id="table-row-5"></tr>
+                <tr id="table-row-6"></tr>
+                <tr id="table-row-7"></tr>
+                <tr id="table-row-8"></tr>
+                <tr id="table-row-9"></tr>
+                <tr id="table-row-10"></tr>
+                <tr id="table-row-11"></tr>
+                <tr id="table-row-12"></tr>
+                <tr id="table-row-13"></tr>
+                <tr id="table-row-14"></tr>
+                <tr id="table-row-15"></tr>
+                <tr id="table-row-16"></tr>
+            </thead>
 
 
-        <script>
-            var events;
-            console.log("events before:" + events);
-            $.getJSON("json/events.json", function(data){
-                events = data;
-                //console.log("events:" + events);
-                console.log("events:" + events[1].sport);
-                console.log("events:" + events[1].city);
+        </table>
 
-                //construct the dates array, and show as table;
-                var text = "<th>sports</th>";
-                var i, j;
-                for (i = 0; i < ((events.length) / 16); i++) {
-                    j = 16 * i;
-                    text += "<th>" + events[j].date + "</th>";
-                    console.log(i + ":" + events[j].date)
-                }
-                console.log(text);
-                document.getElementById("table-head").innerHTML = text;
-
-            });
-        </script>
-
-        <div class="container">
-            <table class="table table-striped">
-                <thead>
-                    <tr id="table-head"></tr>
-                    <tr id="table-row-1"></tr>
-                    <tr id="table-row-2"></tr>
-                    <tr id="table-row-3"></tr>
-                    <tr id="table-row-4"></tr>
-                    <tr id="table-row-5"></tr>
-                    <tr id="table-row-6"></tr>
-                    <tr id="table-row-7"></tr>
-                    <tr id="table-row-8"></tr>
-                    <tr id="table-row-9"></tr>
-                    <tr id="table-row-10"></tr>
-                    <tr id="table-row-11"></tr>
-                    <tr id="table-row-12"></tr>
-                    <tr id="table-row-13"></tr>
-                    <tr id="table-row-14"></tr>
-                    <tr id="table-row-15"></tr>
-                    <tr id="table-row-16"></tr>
-                </thead>
-
-
-            </table>
-        </div>
     </div>
+
+    <script>
+        var events;
+        console.log("events before:" + events);
+        $.getJSON("json/events.json", function(data){
+            events = data;
+            //console.log("events:" + events);
+            console.log("events:" + events[1].sport);
+            console.log("events:" + events[1].city);
+
+            //construct the dates array, and show as table;
+            var text = "<th>sports</th>";
+            var i, j;
+            for (i = 0; i < ((events.length) / 16); i++) {
+                j = 16 * i;
+                text += "<th>" + events[j].date + "</th>";
+                console.log(i + ":" + events[j].date)
+            }
+            console.log(text);
+            document.getElementById("table-head").innerHTML = text;
+
+        });
+    </script>
 </div>
 
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
-        <title>Rome2Rio API</title>
+        <title class="lang" key="title"></title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -20,20 +20,20 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    
+
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
     <!-- Angular -->
-    
-    
+
+
     <!-- Date Picker -->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    
-    
+
+
     <link rel="stylesheet" href="css/style.css">
 
 
