bcdc07a2c48c327f66f51f696eebb9452e894e7e
sculs <liusongscu@gmail.com>
2018-05-18 15:22:30 +0200

    event table head 6

--- resources/views/components/eventsResult.blade.php
+++ resources/views/components/eventsResult.blade.php
@@ -5,40 +5,38 @@
 
 
 
-    </div>
 
-    <script>
-        var events;
-        console.log("events before:" + events);
-        $.getJSON("json/events.json", function(data){
-            events = data;
-            //console.log("events:" + events);
-            console.log("events:" + events[1].sport);
-            console.log("events:" + events[1].city);
+        <script>
+            var events;
+            console.log("events before:" + events);
+            $.getJSON("json/events.json", function(data){
+                events = data;
+                //console.log("events:" + events);
+                console.log("events:" + events[1].sport);
+                console.log("events:" + events[1].city);
 
-            //construct the dates array, and show as table;
-            var text = "";
-            var i, j;
-            for (i = 0; i < ((events.length) / 16); i++) {
-                j = 16 * i;
-                text += "<th>" + events[j].date + "</th>";
-                console.log(i + ":" + events[j].date)
-            }
-            console.log(text);
-            document.getElementById("table-head").innerHTML = text;
+                //construct the dates array, and show as table;
+                var text = "<th>sports</th>";
+                var i, j;
+                for (i = 0; i < ((events.length) / 16); i++) {
+                    j = 16 * i;
+                    text += "<th>" + events[j].date + "</th>";
+                    console.log(i + ":" + events[j].date)
+                }
+                console.log(text);
+                document.getElementById("table-head").innerHTML = text;
 
-        });
-    </script>
+            });
+        </script>
 
         <table class="table table-striped">
             <thead>
-              <tr>
-                <th>sports</th>
-                <span id="table-head"></span>
-              </tr>
+                <tr id="table-head"></tr>
+                <tr id="table-row-1"></tr>
             </thead>
 
 
         </table>
+    </div>
 </div>
 
